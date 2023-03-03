675045c0836baffaeb3976187d4239b2c36735ac
Emery Berger <emery.berger@gmail.com>
2023-03-02 20:35:23 -0500

    Added --all to generate all graphs.

--- conferences.py
+++ conferences.py
@@ -14,6 +14,7 @@ conference_list = conf_data['Conference'].unique().tolist()
 import argparse
 parser = argparse.ArgumentParser(description="Publication analysis for conferences")
 parser.add_argument("--conference_name", help="Name of conference to analyze", choices=conference_list)
+parser.add_argument("--all", dest="all", action="store_const", const=True, help="Produce graphs for all conferences")
 parser.add_argument("--sort", dest="sort", action="store_const", const=True, help="Sort the conferences.csv file.")
 
 args = parser.parse_args()
@@ -27,35 +28,45 @@ if args.sort:
     print(f"Sorted {filename}.")
     sys.exit(0)
     
-if not args.conference_name:
+if not args.conference_name and not args.all:
     parser.print_help()
     sys.exit(0)
 
-# Filter the data by conference
-conference_name = args.conference_name
-conf_data = conf_data.loc[conf_data["Conference"] == conference_name]
+conference_name_list = []
+if args.conference_name:
+    conference_name_list = [args.conference_name]
+if args.all:
+    conference_name_list = conference_list
+    
+full_conf_data = conf_data.copy()
 
-print(conf_data)
+for conference_name in conference_name_list:
+    # Filter the data by conference
+    conf_data = full_conf_data.copy()
+    conf_data = conf_data.loc[conf_data["Conference"] == conference_name]
 
-# Create a bar plot of acceptance rates by year
-#plt.bar(conf_data["Year"], conf_data["Papers accepted"] / conf_data["Papers submitted"])
+    print(conf_data)
 
-# Calculate the number of accepted and rejected papers for each year
-conf_data["Rejected"] = conf_data["Submitted"] - conf_data["Accepted"]
-conf_data["Acceptance Rate"] = conf_data["Accepted"] / conf_data["Submitted"] * 100 # Calculate the acceptance rate
+    # Create a bar plot of acceptance rates by year
+    #plt.bar(conf_data["Year"], conf_data["Papers accepted"] / conf_data["Papers submitted"])
 
-# Create a stacked bar plot of accepted and rejected papers by year
-plt.bar(conf_data["Year"], conf_data["Accepted"], color="green")
-plt.bar(conf_data["Year"], conf_data["Rejected"], bottom=conf_data["Accepted"], label="Rejected", color="red")
+    # Calculate the number of accepted and rejected papers for each year
+    conf_data["Rejected"] = conf_data["Submitted"] - conf_data["Accepted"]
+    conf_data["Acceptance Rate"] = conf_data["Accepted"] / conf_data["Submitted"] * 100 # Calculate the acceptance rate
 
-# Set the x-axis label
-plt.xlabel("Year")
+    # Create a stacked bar plot of accepted and rejected papers by year
+    plt.bar(conf_data["Year"], conf_data["Accepted"], color="green")
+    plt.bar(conf_data["Year"], conf_data["Rejected"], bottom=conf_data["Accepted"], label="Rejected", color="red")
 
-# Set the y-axis label
-plt.ylabel("Accepted / Rejected Papers")
+    # Set the x-axis label
+    plt.xlabel("Year")
 
-# Set the title of the plot
-plt.title(f"{conference_name} Conference Publications by Year")
+    # Set the y-axis label
+    plt.ylabel("Accepted / Rejected Papers")
 
-# Show the plot
-plt.savefig(f"{conference_name}.pdf", bbox_inches="tight")
+    # Set the title of the plot
+    plt.title(f"{conference_name} Conference Publications by Year")
+
+    # Save the plot
+    plt.savefig(f"graphs/{conference_name}.png", bbox_inches="tight")
+    plt.savefig(f"graphs/{conference_name}.pdf", bbox_inches="tight")
